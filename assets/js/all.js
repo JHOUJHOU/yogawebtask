5bbb132d3433a9eba1cb46f93611f6ff37e8f0a6
JHOUJHOU <a01450887@gmail.com>
2021-08-24 23:53:24 +0800

    Update 2021-08-24T15:53:21.180Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,20 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+});
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+});
 //# sourceMappingURL=all.js.map
