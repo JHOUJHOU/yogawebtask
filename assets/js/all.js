00a63e961a7bdff8258a4482a2842cf43eddabae
JHOUJHOU <a01450887@gmail.com>
2021-08-30 14:09:15 +0800

    Update 2021-08-30T06:09:12.512Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -19,4 +19,62 @@ var swiper = new Swiper(".mySwiper1", {
   // },
 
 });
+var constraints = {
+  touch: {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  left: {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  sport: {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  birthdate: {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  name: {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  age: {
+    presence: {
+      message: "是必填欄位"
+    },
+    numericality: {
+      greaterThan: 0,
+      message: "必須大於 0"
+    }
+  },
+  "email": {
+    presence: {
+      message: "是必填的欄位"
+    },
+    email: true
+  }
+};
+var form = document.querySelector("form#myForm");
+var inputs = document.querySelectorAll("input[type=text],input[type=number],select,textarea,checkbox,data");
+inputs.forEach(function (item) {
+  item.addEventListener("click", function () {
+    item.nextElementSibling.textContent = "";
+    var errors = validate(form, constraints);
+    console.log(errors); //呈現在畫面上
+
+    if (errors) {
+      Object.keys(errors).forEach(function (keys) {
+        console.log(keys);
+        document.querySelector(".".concat(keys)).textContent = errors[keys];
+      });
+    }
+  });
+});
 //# sourceMappingURL=all.js.map
