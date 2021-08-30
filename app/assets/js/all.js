3ee98d4bab308a5a1fc439f0c9e9ea8707701a01
JHOUJHOU <a01450887@gmail.com>
2021-08-30 14:09:45 +0800

    立即預約更新

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -19,3 +19,65 @@ var swiper = new Swiper(".mySwiper1", {
   //   clickable: true,
   // },
 });
+
+const constraints = {
+  touch: {
+    presence: {
+      message: "是必填欄位"
+    },
+  },
+  left: {
+    presence: {
+      message: "是必填欄位"
+    },
+  },
+  sport:{
+    presence: {
+      message: "是必填欄位"
+    },
+  },
+  birthdate:{
+    presence: {
+      message: "是必填欄位"
+    },
+  },
+  name:{
+    presence: {
+      message: "是必填欄位"
+    },
+  },
+  age:{
+    presence: {
+      message: "是必填欄位"
+    },
+    numericality: {
+      greaterThan: 0,
+      message: "必須大於 0"
+    },
+  },
+  "email": {  
+    presence:  {
+      message: "是必填的欄位"
+    }, 
+    email: true 
+  },
+};
+
+const form = document.querySelector("form#myForm");
+const inputs = document.querySelectorAll("input[type=text],input[type=number],select,textarea,checkbox,data");
+
+inputs.forEach((item) => {
+
+  item.addEventListener("click", function() {
+    item.nextElementSibling.textContent = "";
+    let errors = validate(form, constraints);
+  console.log(errors)
+    //呈現在畫面上
+    if(errors){
+      Object.keys(errors).forEach(function(keys) {
+        console.log(keys); 
+        document.querySelector(`.${keys}`).textContent = errors[keys]
+      })
+    }
+  });
+});
