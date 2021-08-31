e1ca70d9872505e9e2214a9a3be6b05912063fd9
JHOUJHOU <a01450887@gmail.com>
2021-08-31 21:35:35 +0800

    更新瑜珈空間

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -20,6 +20,15 @@ var swiper = new Swiper(".mySwiper1", {
   // },
 });
 
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
+
 const constraints = {
   touch: {
     presence: {
