0c2caa4578b6fd6b6e5246b0ed68835ad14719c6
JHOUJHOU <a01450887@gmail.com>
2021-08-31 21:34:15 +0800

    Update 2021-08-31T13:34:12.356Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -19,6 +19,14 @@ var swiper = new Swiper(".mySwiper1", {
   // },
 
 });
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "auto",
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+});
 var constraints = {
   touch: {
     presence: {
